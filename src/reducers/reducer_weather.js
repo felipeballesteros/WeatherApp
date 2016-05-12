1d548c6a56d58eaf4eb6c4b2f6efd290d9481bba
felipeballesteros <felipeballesteros@msn.com>
2016-05-12 11:40:50 -0400

    Added case FETCH_WEATHER to the weather reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,21 @@
 
-export default function(state = null, action){
-	console.log('Action Received', action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action){
+
+	switch(action.type){
+
+		case FETCH_WEATHER:
+
+			// never change the existing array, remember, state has to remain the same,
+			// you can add to it, but you cannot modify it
+
+			// These two are the same, the second one is ES6 syntax
+			//return state.concat([action.payload.data]);
+			return [ action.payload.data, ...state ]; // [ city, city, city ]
+
+
+	}
 
 	return action;
 }
